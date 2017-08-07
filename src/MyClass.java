080b1c8cef0d0a2895e06411788ec9b07d577e57
J Cheyo Jimenez <cheyo@masters3d.com>
2017-08-06 17:48:41 -0700

    init

---
+++ src/MyClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author cheyo
+ *
+ */
+public final class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+		System.out.print("Added another line");
+
+	}
+
+}
